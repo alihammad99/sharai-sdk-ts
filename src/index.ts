d22f982a37ecf8380045c0fddede763a63d1e5bd
Ali Hammad <aliressan.ar@gmail.com>
2024-08-17 16:14:25 +0300

    env path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: "../../.env" });
+const config = {
+  envPath: "../.env",
+};
+dotenv.config({ path: path.resolve(__dirname, config.envPath) });
+
+
 export { default as Product } from "./product";
 export { default as Invoice } from "./invoice";
 export { default as Category } from "./category";
