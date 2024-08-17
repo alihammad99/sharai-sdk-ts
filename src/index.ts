0d9ef3dc8a7c823611495c15ea17b087f482e011
Ali Hammad <aliressan.ar@gmail.com>
2024-08-17 13:38:55 +0300

    minify

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config'
 export { default as Product } from "./product";
 export { default as Invoice } from "./invoice";
 export { default as Category } from "./category";
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"), // Specify your entry file
       name: "sharai-sdk-ts", // Name of the global variable if you're building a library
-      fileName: (format) => `main.${format}.js`, // Output file naming convention
+      fileName: "main", // Output file naming convention without format
+      formats: ["umd"], // Specify that you only want the UMD format
     },
     rollupOptions: {
       // Externalize dependencies that shouldn't be bundled
